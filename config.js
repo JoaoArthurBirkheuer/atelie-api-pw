248b7a5a54f20b5fd16d5874f8c07591e2e19fa2
JOÃO ARTHUR BIRKHEUER <joaobirkheuer@gmail.com>
2025-05-08 00:19:50 -0300

    Mudança em config para deploy

--- config.js
+++ config.js
@@ -3,23 +3,13 @@ const { Pool } = require('pg');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-let pool;
-
-if (isProduction) {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-} else {
-  pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-  });
-}
+const pool = new Pool({
+  connectionString: isProduction
+    ? process.env.DATABASE_URL
+    : `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = { pool };
